2ee54662a2d9c3c1eaae59d7767767452a9f4f1b
anubra266 <anubra266@gmail.com>
2021-05-03 04:37:37 +0100

    🐛 Fix tests

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -2,6 +2,6 @@ import Progress from '../src';
 
 describe('blah', () => {
   it('works', () => {
-    expect(Progress.init).toEqual(null);
+    expect(Progress.init()).toEqual(undefined);
   });
 });
